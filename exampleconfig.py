de5b16e4109debcf631726e98a25702aaa5c2ba2
LAPTOP-G43MA5MO\ekarni <eyalk5@gmail.com>
2021-10-13 00:48:22 +0300

    updated example config

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,18 +1,25 @@
 import datetime
 
+from common import InputSourceType, UseCache
+
+TZINFO=datetime.timezone(datetime.timedelta(hours=-3),'GMT3')
 MAXCACHETIMESPAN=datetime.timedelta(days=1)
 HIST_F = r'hist_file.cache'
-REGULAR_ACCOUNT = 'Account Number in IB'
-REGULAR_USERNAME = 'User name in ib'
-PORT = 5050
-GROUPS= {'Chips': ['NVDA', 'AMD', 'CLOU', 'ARKG', 'ARKK']
+PORT = 4001#5050
+EXT=['QQQ']
+DEF_FIG_SIZE = (13.2,6)
+GROUPS= {'Tech': ['NVDA', 'AMD', 'CLOU', ]
         , 'FANG': ['FB', 'AAPL', 'GOOGL', 'AMZN']
-        , 'crypt': ['ethereum', 'cardano', 'bitcoin']
-        , 'broadec': ['VO', 'VPU', 'GSG', ]
-        , 'anotherus': [ 'MSOS', 'MAC']
-        , 'china': ['ADRE']
-        , 'europe': ['EWA', 'EWG', 'EZU', 'EDEN']
-        , 'moneyothers': ['TGT' 'FVRR', 'SQ', 'TSLA'],
-              'med': ['NVAX', 'BNTX', 'MRNA', 'REGN']}
+        , 'Crypto': ['ethereum', 'cardano', 'bitcoin']
+        , 'BroadEconomics': ['VO', 'VPU', 'GSG', ]
+        , 'Alt Stocks': [ 'MSOS', 'MAC','ARKG', 'ARKK']
+        , 'China': ['ADRE']
+        , 'Europe': ['EWA', 'EWG', 'EZU', 'EDEN']
+        , 'Money Others': ['TGT' 'FVRR', 'SQ', 'TSLA'],
+        'Pharma': ['NVAX', 'BNTX', 'MRNA', 'REGN']}
+EXCHANGES= ["nasdaq","xetra",'NYSE','OTC Markets']
+EXCHANGES= [i.lower() for i in EXCHANGES]
+INPUTSOURCE=InputSourceType.InvestPy
+FN= r'prot2.csv'
 DEF_PORTFOLIO = 'My Portfolio'
-FN="Csv From MyStockProtofolio (buy/sells)"
+CACHEUSAGE=UseCache.USEIFAVALIABLE
